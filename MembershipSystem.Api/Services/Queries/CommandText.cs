fd318da9fadf509913d62fe272f1145b49f3e296
Muhammad Umar Zahir <muhammad.zahir@sainsburys.co.uk>
2020-06-22 16:49:34 +0100

    Fixed create new user query

--- MembershipSystem.Api/Services/Queries/CommandText.cs
+++ MembershipSystem.Api/Services/Queries/CommandText.cs
@@ -5,6 +5,6 @@
         public string GetUserByCardId => "Select * from CardUsers where CardId= @CardId";
 
         public string AddUser => "INSERT INTO CardUsers (CardId, EmployeeId, Name, Email, Mobile) " +
-            "VALUES ('FyDJ0lbYcPkzp2Ja', @EmployeeId, @Name, @Email, Mobile) ";
+            "VALUES ('@CardId', @EmployeeId, @Name, @Email, @Mobile) ";
     }
 }
